aef7579a8f7845e87cbf89260e0f2f84f2b9dbd0
c++junkie <asolinks@hotmail.com>
2025-06-24 05:13:03 +0300

    Lab on Introduction to AI and Machine Learning on Google Cloud

---
+++ googleMLOPS/1._Introduction_to_AI.sql
@@ -0,0 +1,70 @@
+#Task 2. Explore the data
+#standardSQL
+SELECT
+  IF(totals.transactions IS NULL, 0, 1) AS label,
+  IFNULL(device.operatingSystem, "") AS os,
+  device.isMobile AS is_mobile,
+  IFNULL(geoNetwork.country, "") AS country,
+  IFNULL(totals.pageviews, 0) AS pageviews
+FROM
+  `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+WHERE
+  _TABLE_SUFFIX BETWEEN '20160801' AND '20170631'
+LIMIT 10000;
+
+
+#Task 3. Create a model
+#standardSQL
+CREATE OR REPLACE MODEL `bqml_lab.sample_model`
+OPTIONS(model_type='logistic_reg') AS
+SELECT * from `bqml_lab.training_data`;
+
+
+#Task 4. Evaluate the Model
+#standardSQL
+SELECT
+  *
+FROM
+  ml.EVALUATE(MODEL `bqml_lab.sample_model`);
+
+
+  #Task 5. Use the model
+  #standardSQL
+SELECT
+  IF(totals.transactions IS NULL, 0, 1) AS label,
+  IFNULL(device.operatingSystem, "") AS os,
+  device.isMobile AS is_mobile,
+  IFNULL(geoNetwork.country, "") AS country,
+  IFNULL(totals.pageviews, 0) AS pageviews,
+  fullVisitorId
+FROM
+  `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+WHERE
+  _TABLE_SUFFIX BETWEEN '20170701' AND '20170801';
+
+
+
+  #Predict purchases per country/region
+  #standardSQL
+SELECT
+  country,
+  SUM(predicted_label) as total_predicted_purchases
+FROM
+  ml.PREDICT(MODEL `bqml_lab.sample_model`, (
+SELECT * FROM `bqml_lab.july_data`))
+GROUP BY country
+ORDER BY total_predicted_purchases DESC
+LIMIT 10;
+
+
+#Predict purchases per user
+#standardSQL
+SELECT
+  fullVisitorId,
+  SUM(predicted_label) as total_predicted_purchases
+FROM
+  ml.PREDICT(MODEL `bqml_lab.sample_model`, (
+SELECT * FROM `bqml_lab.july_data`))
+GROUP BY fullVisitorId
+ORDER BY total_predicted_purchases DESC
+LIMIT 10;
